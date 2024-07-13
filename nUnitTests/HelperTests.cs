6354320fd69ded1975c90c37536dc94634cc7a8b
Hossrod <hossrod@gmail.com>
2024-07-13 13:22:13 -0700

    Refactored method and class names so hierarchy in test runners are better structured and are auto generated without use of attributes.

--- nUnitTests/HelperTests.cs
+++ nUnitTests/HelperTests.cs
@@ -1,15 +1,14 @@
 namespace SerialMonitor.nUnitTests
 {
-    public class Helpers_ConvertBytesToHumanReadable_Method
+    [TestFixture(Description = "Tests the Helpers.ConvertBytesToHumanReadable Method.")]
+    public class Helpers_ConvertBytesToHumanReadable
     {
-        [Test]
-        [Category("Functional Tests")]
         [TestCase(512, "512 bytes")] 
         [TestCase(1024, "1 KB")] 
         [TestCase(1048576, "1 MB")] 
         [TestCase(1073741824, "1 GB")] 
         [TestCase(1099511627776, "1 TB")] 
-        public void Functional(long bytes, string expected)
+        public void Functional_Testing(long bytes, string expected)
         {
             // Act
             string result = Helpers.ConvertBytesToHumanReadable(bytes);
@@ -22,8 +21,7 @@ namespace SerialMonitor.nUnitTests
         }
 
         [Test]
-        [Category("Out of Range Tests")]
-        public void NegativeBytes()
+        public void OutOfRangeTesting_NegativeBytes()
         {
             // Arrange
             long bytes = -1024; // Negative bytes
@@ -40,8 +38,7 @@ namespace SerialMonitor.nUnitTests
         }
 
         [Test]
-        [Category("Boundry Tests")]
-        public void ZeroBytes()
+        public void BoundryTesting_ZeroBytes()
         {
             // Arrange
             long bytes = 0;
@@ -58,8 +55,7 @@ namespace SerialMonitor.nUnitTests
         }
 
         [Test]
-        [Category("Boundry Tests")]
-        public void MaxLong()
+        public void BoundryTesting_MaxLong()
         {
             // Arrange
             long bytes = long.MaxValue; // Maximum possible long value
@@ -69,17 +65,15 @@ namespace SerialMonitor.nUnitTests
             string result = Helpers.ConvertBytesToHumanReadable(bytes);
 
             // Assert
-            // We don't know the exact expected result, but we can at least assert that it ends with "TB"
+            // Value can vary, but we can at least assert that it ends with "TB"
             StringAssert.EndsWith(expected, result, $"Input was '{bytes}', ");
         }
 
-        [Test]
-        [Category("Edge Tests")]
         [TestCase(1023, "1023 bytes")] // Maximum bytes before KB
         [TestCase(1048575, "1024 KB")] // Maximum bytes before MB
         [TestCase(1073741823, "1024 MB")] // Maximum bytes before GB
         [TestCase(1099511627775, "1024 GB")] // Maximum bytes before TB
-        public void Boundary(long bytes, string expected)
+        public void Edge_Testing(long bytes, string expected)
         {
             // Act
             string result = Helpers.ConvertBytesToHumanReadable(bytes);
