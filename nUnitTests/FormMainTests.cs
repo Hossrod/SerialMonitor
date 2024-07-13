a08a07b3077d82f6bde043e527f83070062485a7
Hossrod <hossrod@gmail.com>
2024-07-13 13:16:06 -0700

    Refactored method and class names so hiearchy in test runners are better structured and are auto generated without use of attributes.

---
+++ nUnitTests/FormMainTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace SerialMonitor.nUnitTests
+{
+    [TestFixture(Description = "Tests for the FormatRule class.")]
+    public class FormMain_FormatRule
+    {
+        [Test]
+        public void Constructor_SetsPropertiesCorrectly()
+        {
+            // Arrange
+            string searchTerm = "test";
+            Color termColor = Color.Red;
+            bool caseSensitive = true;
+
+            // Act
+            FormatRule rule = new FormatRule(searchTerm, termColor, caseSensitive);
+
+            // Assert
+            Assert.That(rule.SearchTerm, Is.EqualTo(searchTerm));
+            Assert.That(rule.TermColor, Is.EqualTo(termColor));
+            Assert.That(rule.CaseSensitive, Is.EqualTo(caseSensitive));
+        }
+
+        [Test]
+        public void Constructor_SetsCaseSensitiveToFalseByDefault()
+        {
+            // Arrange
+            string searchTerm = "test";
+            Color termColor = Color.Red;
+
+            // Act
+            FormatRule rule = new FormatRule(searchTerm, termColor);
+
+            // Assert
+            Assert.That(rule.SearchTerm, Is.EqualTo(searchTerm));
+            Assert.That(rule.TermColor, Is.EqualTo(termColor));
+            Assert.That(rule.CaseSensitive, Is.False);
+        }
+    }
+}
